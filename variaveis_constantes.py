176b133293bfc7bc36005ad32cd29b10fee3741b
andremattos <andrefilipe90@gmail.com>
2024-04-23 19:22:27 -0300

    Update: sync with MacOS

---
+++ variaveis_constantes.py
@@ -0,0 +1,8 @@
+nome = "André"
+idade = 33
+print(nome,idade)
+nome = "André Mattos"
+print(nome,idade)
+limite_saque_diario = 1000
+print(limite_saque_diario)
+print(f"Meu nome é {nome} e tenho {idade} anos.")
