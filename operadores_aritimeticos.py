b0cc45451bb1faffeebe7580d88dad793fcc8341
andrefilipe90 <andrefilipe90@gmail.com>
2024-04-23 19:35:12 -0300

    Update: Operadores Aritimeticos

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,10 @@
+produto_1 = 10
+produto_2 = 20
+
+print (produto_1 + produto_2)
+print (produto_1 - produto_2)
+print (produto_2 / produto_1)
+print (produto_2 // produto_1)
+
+x = 10 + (5 * 4)
+print(x)
