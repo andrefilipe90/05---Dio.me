eed686ba8fbfc79301a596f844be0a95a580e47a
andrefilipe90 <andrefilipe90@gmail.com>
2024-04-23 20:19:09 -0300

    Update: Operadores Associacao

---
+++ operadores_associacao.py
@@ -0,0 +1,8 @@
+curso = "Curso de Python"
+frutas = ["Laranja","Uva","Limão"]
+saques =[1500,100]
+
+print("Python" in curso)
+print("Py" in curso)
+print("Laranja"not in frutas)
+print("La" in frutas) ## dentro do item que está dentro de frutas não consigo verificar parte de uma string como em uma variavel unica *curso*
