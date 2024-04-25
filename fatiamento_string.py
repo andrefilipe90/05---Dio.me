a6e4e67c07c159daea0a0c2ea05a95288020f994
andrefilipe90 <andrefilipe90@gmail.com>
2024-04-25 16:27:48 -0300

    string multiplas linhas

--- fatiamento_string.py
+++ fatiamento_string.py
@@ -1,15 +1,19 @@
 nome = "André Filipe Guering de Mattos"
 
-print(nome[0])
+#print(nome[0])
+#print(nome[:9])
+#print(nome[10:])
+#print(nome[10:16])
+#print(nome[10:16:2])
+#print(nome[:])
+#print(nome[::-1])
 
-print(nome[:9])
+nome = " André "
+sobrenome = " Mattos "
 
-print(nome[10:])
+mensagem = f'''
+{nome.center(20,"#")}
+{sobrenome.center(20,"#")}
+'''
 
-print(nome[10:16])
-
-print(nome[10:16:2])
-
-print(nome[:])
-
-print(nome[::-1])
+print (mensagem)
